3386ab04bef7bf70665fe04c7393cf1e4c36052f
Facucode <34733879+Facucode@users.noreply.github.com>
2019-12-18 20:05:16 -0300

    Adding styles

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,40 @@
 import React, {Component} from 'react';
-
+import './Task.css'
 class Task extends Component{
+
+    StyleCompleted(){
+        return{
+            fontSize:'20px',
+            color:this.props.task.done ? 'gray' : 'black',
+            textDecoration:this.props.task.done ? 'line-through' : 'none'
+        }
+    }
 
     render(){
         const {task}=this.props;
-
-        return <div> 
+        //const redColor = {background:'red'}
+        return <p style={this.StyleCompleted()}> 
         {task.title} - 
         {task.description} - 
         {task.done} - 
         {task.id}
         <input type="checkbox" />
-        <button>
+        <button style={btnDelete}>
             x
         </button>
-        </div>
+        </p>
     }
+}
+
+const btnDelete = {
+    fontSize: '18px',
+    background:'#ea2027',
+    color:'#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius:'50%',
+    cursor:'pointer',
+    float:'right'
 }
 
 export default Task
